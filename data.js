a53c03015f39f150374c4c49e391f59d21dc86de
modnayalavkaroma <modnayalavka.roma@gmail.com>
2026-03-30 10:38:15 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+function Header() {
+  return `
+    <div class="header">
+      <img src="https://img.bellavka.by/b/8/82/822/8223/82231.png">
+      <b>Firmova Ads</b>
+    </div>
+  `;
+}
+
+function Card(content) {
+  return `<div class="card">${content}</div>`;
+}
